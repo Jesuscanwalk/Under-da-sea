b5da6bf49968f64cee52ebc0535f1f2fd5e574ef
Jesuscanwalk <159198277+Jesuscanwalk@users.noreply.github.com>
2025-03-29 22:24:57 -0700

    fish are in groups amnd travel together yippe!

--- under-da-sea/Fish/test_2.gd
+++ under-da-sea/Fish/test_2.gd
@@ -2,14 +2,22 @@ extends Node2D
 
 @export var num := 100
 @export var margin := 100
-var screensize : Vector2 
+var screensize = get_viewport_rect().size 
+var fish_scene = preload("res://Fish/fish.tscn")
+var num_groups = 5
+var fish_per_group = 10
+var group_radius = 50
 
 func _ready() -> void:
-	screensize = get_viewport_rect().size
-	randomize()
 	for i in num:
 		spawnFish()
-
+	for i in range(num_groups):
+		var group_center = Vector2(randf_range(0, screensize.x), randf_range(0, screensize.y))
+		for j in range(fish_per_group):
+			var fish_instance = fish_scene.instantiate()
+			var random_offset = Vector2(randf_range(-group_radius, group_radius), randf_range(-group_radius, group_radius))
+			fish_instance.global_position = group_center + random_offset
+			add_child(fish_instance)
 
 func spawnFish():
 	var fish : Area2D = preload("res://Fish/fish.tscn").instantiate()

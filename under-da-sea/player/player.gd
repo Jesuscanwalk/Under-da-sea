45d234c459a47db0cdc45c1b09439e4cd334770f
maauyu <159198030+maauyu@users.noreply.github.com>
2025-04-11 17:07:32 -0700

    CAN SWIM!!!!!

--- under-da-sea/player/player.gd
+++ under-da-sea/player/player.gd
@@ -5,11 +5,15 @@ class_name Player extends CharacterBody2D
 @export var max_health := 5
 @onready var animated_sprite: AnimatedSprite2D = $AnimationPlayer
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+@export var SPEED : float = 130.0
+@export var JUMP_VELOCITY : float = -300.0
+@export var SWIM_GRAVITY : float = 0.25
+@export var SWIM_VELOCITY : float = 80
+@export var SWIM_JUMP : float = -100
 
 var health := max_health: set = set_health
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_in_water : bool = false
 
 func _ready() -> void:
 	health_bar.max_value = max_health
@@ -17,7 +21,15 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if(!is_in_water):
+			velocity.y += gravity * delta
+		else:
+			velocity.y = clampf(velocity.y + (gravity * delta * SWIM_GRAVITY), -10000, SWIM_VELOCITY)
+	if Input.is_action_just_pressed("up"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_in_water == true:
+			velocity.y += SWIM_JUMP
 	var direction = Input.get_axis("left", "right")
 	if direction > 0:
 		animated_sprite.flip_h = true
@@ -45,3 +57,8 @@ func die() -> void:
 	queue_free()
 	collision_shape_2d.set_deferred("disabled", true)
 	get_tree().change_scene_to_file("res://menus/end.tscn")
+
+
+func _on_water_detection_water_state_changed(is_in_water):
+	self.is_in_water = is_in_water
+	print("is in water")

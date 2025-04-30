6b7aae133e513a4c86ba1ed94993bcb91a86844a
maauyu <159198030+maauyu@users.noreply.github.com>
2025-04-30 12:48:33 -0700

    igloo + getting inside it

---
+++ under-da-sea/map/igloo.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var is_player_near := false
+
+func _ready() -> void:
+	body_entered.connect(func (body: Node) -> void:
+		is_player_near = true
+			)
+	body_exited.connect(func (body:Node) -> void:
+		is_player_near = false
+		)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if is_player_near and event.is_action_pressed("interact"):
+		get_tree().change_scene_to_file("res://inside.tscn")
+		set_process_unhandled_input(false)

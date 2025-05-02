a365cd3444c8c90f33b0da52e7f70b6c32ca8269
Jesuscanwalk <159198277+Jesuscanwalk@users.noreply.github.com>
2025-05-02 14:55:46 -0700

    Reapply "Merge branch 'main' into forcing-fish-inside-the-area"
    This reverts commit ee9a63ac77b17ae5bcd333a1e68669e27a46b999.

--- under-da-sea/player/player.gd
+++ under-da-sea/player/player.gd
@@ -1,23 +1,38 @@
 class_name Player extends CharacterBody2D
 
-@onready var health_bar: ProgressBar = $Control/HealthBar
+signal health_changed
+
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@export var max_health := 5
 @onready var animated_sprite: AnimatedSprite2D = $AnimationPlayer
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+@export var SPEED : float = 130.0
+@export var JUMP_VELOCITY : float = -250
+@export var SWIM_GRAVITY : float = 0.25
+@export var SWIM_VELOCITY : float = 80
+@export var SWIM_JUMP : float = -200
 
-var health := max_health: set = set_health
+@export var max_health = 3
+@onready var current_health: int = max_health
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_in_water : bool = false
 
 func _ready() -> void:
-	health_bar.max_value = max_health
-	health_bar.value = health
+	pass
 
 func _physics_process(delta):
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if(!is_in_water):
+			velocity.y += gravity * delta
+			animated_sprite.play("jump")
+		else:
+			velocity.y = clampf(velocity.y + (gravity * delta * SWIM_GRAVITY), -10000, SWIM_VELOCITY)
+			animated_sprite.play("swim")
+	if Input.is_action_just_pressed("up"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_in_water == true:
+			velocity.y += SWIM_JUMP
 	var direction = Input.get_axis("left", "right")
 	if direction > 0:
 		animated_sprite.flip_h = true
@@ -33,15 +48,25 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
-
-func set_health(new_health: int) -> void:
-	var previous_health := health
-	health = clampi(new_health, 0, max_health)
-	health_bar.value = health
-	if health == 0:
-		die()
+	handle_collision()
 
 func die() -> void:
 	queue_free()
 	collision_shape_2d.set_deferred("disabled", true)
 	get_tree().change_scene_to_file("res://menus/end.tscn")
+
+func handle_collision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+
+func _on_water_detection_water_state_changed(is_in_water):
+	self.is_in_water = is_in_water
+	print(is_in_water)
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if area.name == "HitBox":
+		current_health -= 1
+	if current_health == 0:
+			die()
+	health_changed.emit(current_health)
